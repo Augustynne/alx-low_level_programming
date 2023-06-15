097632c32b15b7aa7ccde458788adefdfd848f59
Augustynne <nana__austin@yahoo.com>
2023-06-15 13:14:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 /**
 *main-a program that prints a line with a puts function
 *Return: 0(You've succeeded)
-*\
-int main(void);{
+*/
+int main(void);
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
